bad3784ee86013b40607582dfda5f9c64ce66bfd
JoseFlores <corazon_oro72@hotmail.com>
2018-04-24 23:53:37 -0500

    Secciones

--- app/src/main/java/com/example/joseflores/seminario/MainActivity.java
+++ app/src/main/java/com/example/joseflores/seminario/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.joseflores.seminario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNext = (Button) findViewById(R.id.btnSiguiente);
+        btnNext.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.btnSiguiente:
+
+                startActivity(new Intent(MainActivity.this, SeccionActivity.class));
+
+                break;
+        }
     }
 }
